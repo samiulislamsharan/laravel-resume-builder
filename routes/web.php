51f50b2cfcde9e910e9365a2bd79367a8000f76e
Samiul Islam <samiulislamsharan@gmail.com>
2024-04-28 02:17:32 +0600

    feat(routes): add profile and user-detail routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserDetailController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,13 +20,18 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('main');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/user-detail/create', [UserDetailController::class, 'create'])->name('detail.create');
+    Route::post('/user-detail/store', [UserDetailController::class, 'store'])->name('detail.store');
 });
 
 require __DIR__ . '/auth.php';
