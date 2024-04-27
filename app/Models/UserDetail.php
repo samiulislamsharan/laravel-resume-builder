5db0a25eeb057ecc474a658784b690547abc6cec
Samiul Islam <samiulislamsharan@gmail.com>
2024-04-28 02:23:23 +0600

    feat(models): add UserDetail model with fillable fields

---
+++ app/Models/UserDetail.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserDetail extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'full_name',
+        'email',
+        'phone_number',
+        'address',
+    ];
+}
