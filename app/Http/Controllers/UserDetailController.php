a006b7ddc6c05ee02a2bfe08f80e43a8c06b0e99
Samiul Islam <samiulislamsharan@gmail.com>
2024-04-28 02:24:05 +0600

    feat(controllers): add create and store methods in UserDetailController

---
+++ app/Http/Controllers/UserDetailController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserDetail;
+use Illuminate\Http\Request;
+
+class UserDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('user_detail.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'full_name' => 'required',
+            'email' => 'required|email',
+            'phone_number' => 'required',
+        ]);
+
+        $userDetail = new UserDetail();
+
+        $userDetail->user_id = auth()->id();
+        $userDetail->full_name = $request->full_name;
+        $userDetail->email = $request->email;
+        $userDetail->phone_number = $request->phone_number;
+        $userDetail->address = $request->address;
+        $userDetail->save();
+
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserDetail $userDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UserDetail $userDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UserDetail $userDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UserDetail $userDetail)
+    {
+        //
+    }
+}
