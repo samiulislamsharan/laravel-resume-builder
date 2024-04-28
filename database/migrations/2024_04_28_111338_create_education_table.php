11730ae7cd83345bd6bde43b262a1f3646d4ad98
Samiul Islam <samiulislamsharan@gmail.com>
2024-04-29 01:13:29 +0600

    feat(migrations): :sparkles: create migration for education table

---
+++ database/migrations/2024_04_28_111338_create_education_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('education', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('school_name');
+            $table->string('degree');
+            $table->string('field_of_study')->nullable();
+            $table->date('start_date');
+            $table->date('end_date');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('education');
+    }
+};
