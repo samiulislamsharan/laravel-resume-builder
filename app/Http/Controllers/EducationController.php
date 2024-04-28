efde35fa9d3d59a39b781cf289a006c9b15e2214
Samiul Islam <samiulislamsharan@gmail.com>
2024-04-29 01:12:17 +0600

    feat(controllers): :sparkles: add EducationController with index, create, store, show, edit, update, and destroy methods

---
+++ app/Http/Controllers/EducationController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Education;
+use Illuminate\Http\Request;
+
+class EducationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $education = auth()->user()->education;
+
+        return view('education.index', [
+            'educations' => $education,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('education.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'school_name' => 'required',
+            'degree' => 'required',
+            'field_of_study' => 'required',
+            'start_date' => 'required',
+            'end_date' => 'required',
+        ]);
+
+        auth()->user()->education()->create($request->all());
+
+        return redirect()->route('education.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Education $education)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Education $education)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Education $education)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Education $education)
+    {
+        //
+    }
+}
