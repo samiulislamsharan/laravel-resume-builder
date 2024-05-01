abab5ad64eac735c2d92a949965bccea72aca42e
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-01 18:09:59 +0600

    feat(laravel): :sparkles: add Skill model, controller and migration

---
+++ app/Http/Controllers/SkillController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Skill;
+use Illuminate\Http\Request;
+
+class SkillController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $skills = auth()->user()->skills;
+
+        return view('skill.index', compact('skills'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('skill.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $formData = $request->validate([
+            'name' => 'required',
+            'rating' => 'required'|'numeric'|'min:1'|'max:5',
+        ]);
+
+        auth()->user()->skills()->create($formData);
+
+        return redirect()->route('skill.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Skill $skill)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Skill $skill)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Skill $skill)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Skill $skill)
+    {
+        //
+    }
+}
