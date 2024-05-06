f6b3a13fb07890e6748093c6a46f19de664e77a5
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-06 22:44:13 +0600

    refactor(views): :lipstick: update main.blade.php with improved formatting and link for creating a new resume

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,11 +6,15 @@
     <div class="container px-4 py-8 mx-auto">
         <div class="flex flex-col items-center">
             <h1 class="text-4xl font-bold text-gray-800 dark:text-gray-100">Resume Builder</h1>
-            <p class="text-gray-600 dark:text-gray-400">Create your resume in minutes</p>
+            <p class="text-gray-600 dark:text-gray-400">
+                Create your resume in minutes
+            </p>
         </div>
         <div class="flex flex-col items-center mt-8">
-            <a href="{{ route('user-details.create') }}" class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">Create
-                a new resume</a>
+            <a href="{{ route('user-detail.create') }}"
+                class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
+                Create a new resume
+            </a>
         </div>
     </div>
 @endsection
