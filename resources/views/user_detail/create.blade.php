f9db456a64e446d44fb2d092563fe3b6a195fe62
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-06 22:50:19 +0600

    refactor(views): Add summary field to create.blade.php for user details

--- resources/views/user_detail/create.blade.php
+++ resources/views/user_detail/create.blade.php
@@ -93,6 +93,27 @@
                     </div>
                 </div>
 
+                <div class="flex flex-wrap mb-6 -mx-3">
+                    <div class="w-full px-3">
+                        <label for="summary" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Summary
+                        </label>
+
+                        <div class="flex">
+                            <span
+                                class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-gray-300 border-e-0 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                                <i class="fa-solid fa-file"></i>
+                            </span>
+                            <textarea name="summary" id="summary"
+                                class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Summary"></textarea>
+                            @error('summary')
+                                <p class="mt-2 text-xs italic text-red-500">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+
                 <button type="submit"
                     class="px-4 py-2 mt-4 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">Submit</button>
             </form>
